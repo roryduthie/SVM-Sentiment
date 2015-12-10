56d3e02a9f1c0b6ee82b279e9050c43ebad470a7
roryduthie <roryduthie@gmail.com>
2015-12-10 11:59:41 +0000

    Remove Stopwords New Vocab

--- ClassifyText/Program.cs
+++ ClassifyText/Program.cs
@@ -2,6 +2,8 @@
 using libsvm;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,8 +17,16 @@ namespace ClassifyText
 
         static void Main()
         {
-            const string dataFilePath = @"C:\Users\roryduthie\Desktop\Han.csv";
-
+            const string dataFilePath = @"C:\Users\Rory\Desktop\Han.csv";
+            List<String> negwords = new List<String>();
+            List<String> poswords = new List<String>();
+            String negFile = "C:/Users/Rory/Desktop/negative-words.txt";
+            String posFile = "C:/Users/Rory/Desktop/positive-words.txt";
+            GetNeg(negwords, negFile);
+            GetNeg(poswords, posFile);
+           
+            String[] stopwords = new String[]{"hon.","gentleman","member","friend","lady","a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as",  "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "i","ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"};
+            List<String> stop = stopwords.ToList<String>();
             var dataTable = DataTable.New.ReadCsv(dataFilePath);
             List<string> x = dataTable.Rows.Select(row => row["Text"]).ToList();
             double[] y = dataTable.Rows.Select(row => double.Parse(row["IsPos"])).ToArray();
@@ -26,22 +36,50 @@ namespace ClassifyText
             Bigram b = new Bigram();
             List<String> v = new List<string>();
 
+            String sent = "";
+
             for(int i = 0; i < x.Count; i++)
             {
                 String c = x[i].ToString();
+                
                 c = c.Replace(",", "");
-                v.AddRange(b.getNG(c));
+                c = c.ToLower();
+                String[] sp = c.Split(' ');
+                for (int z = 0; z < sp.Length; z++)
+                {
+                    String word = sp[z];
+                    if(stop.Contains(word))
+                    {
+                        Debug.WriteLine("Stop Word");
+                    }
+                    else
+                    {
+                        sent += word;
+                        sent += " ";
+                    }
+                }
+
+                sent = sent.Trim();
+                
+                v.AddRange(b.getNG(sent));
+                
+
+                sent = "";
             }
-
+            int bound = v.Count();
+          //v.AddRange(negwords);
+            int nBound = v.Count();
+          //v.AddRange(poswords);
+            //v = v.Distinct().ToList();
             var problemBuilder = new TextClassificationProblemBuilder();
-            var problem = problemBuilder.CreateProblem(x, y, v);
+            var problem = problemBuilder.CreateProblem(x, y, v, bound, nBound);
 
-            ProblemHelper.WriteProblem(@"C:\Users\roryduthie\Desktop\hanData.problem", problem);
+            ProblemHelper.WriteProblem(@"C:\Users\Rory\Desktop\hanData.problem", problem);
 
-            problem = ProblemHelper.ReadProblem(@"C:\Users\roryduthie\Desktop\hanData.problem");
+            problem = ProblemHelper.ReadProblem(@"C:\Users\Rory\Desktop\hanData.problem");
 
             const int C = 1;
-            var model = new C_SVC(problem, KernelHelper.LinearKernel(), C);
+            var model = new C_SVC(problem, KernelHelper.RadialBasisFunctionKernel(1), C);
 
             var accuracy = model.GetCrossValidationAccuracy(10);
             Console.WriteLine("Accuracy of the model is {0:P}", accuracy); 
@@ -52,7 +90,7 @@ namespace ClassifyText
             do
             {
                 userInput = Console.ReadLine();
-                var newX = TextClassificationProblemBuilder.CreateNode(userInput, v);
+                var newX = TextClassificationProblemBuilder.CreateNode(userInput, v, bound, nBound);
                 var predictedY = model.Predict(newX);
 
                 Console.WriteLine("The prediction is {0}", _predictionDictionary[(int)predictedY]);
@@ -67,6 +105,22 @@ namespace ClassifyText
         private static IEnumerable<string> GetWords(string x)
         {
             return x.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public static List<String> GetNeg(List<String> words, String file)
+        {
+            using(StreamReader r = new StreamReader(file))
+            {
+                String line;
+
+                while((line = r.ReadLine()) != null)
+                {
+                    words.Add(line);
+                }
+
+            }
+
+            return words;
         }
     }
 }
--- ClassifyText/TextClassificationProblemBuilder.cs
+++ ClassifyText/TextClassificationProblemBuilder.cs
@@ -8,32 +8,47 @@ namespace ClassifyText
 {
     class TextClassificationProblemBuilder
     {
-        public svm_problem CreateProblem(IEnumerable<string> x, double[] y, IReadOnlyList<string> vocabulary)
+        public svm_problem CreateProblem(IEnumerable<string> x, double[] y, IReadOnlyList<string> vocabulary, int bound, int nb)
         {
+            int b = bound;
+            int n = nb;
             return new svm_problem
             {
+                
                 y = y,
-                x = x.Select(xVector => CreateNode(xVector, vocabulary)).ToArray(),
+                x = x.Select(xVector => CreateNode(xVector, vocabulary, b, n)).ToArray(),
                 l = y.Length
             };
         }
 
-        public static svm_node[] CreateNode(string x, IReadOnlyList<string> vocabulary)
+        public static svm_node[] CreateNode(string x, IReadOnlyList<string> vocabulary, int bound, int nBound)
         {
             var node = new List<svm_node>(vocabulary.Count);
+            int sum = 0;
             List<string> allWords = new List<string>();
             x = x.Replace(",", "");
             Bigram b = new Bigram();
 
             allWords = b.getNG(x);
 
-            string[] words = x.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] words = allWords.ToArray();
 
             for (int i = 0; i < vocabulary.Count; i++)
             {
-                int occurenceCount = allWords.Count(s => String.Equals(s, vocabulary[i], StringComparison.OrdinalIgnoreCase));
+                int occurenceCount = words.Count(s => String.Equals(s, vocabulary[i], StringComparison.OrdinalIgnoreCase));
                 if (occurenceCount == 0)
                     continue;
+
+                sum = i + 1;
+                if(sum > bound && sum < nBound)
+                {
+                    occurenceCount = -1;
+                    sum = 0;
+                }
+                if(sum > nBound)
+                {
+                    occurenceCount = 1;
+                }
 
                 node.Add(new svm_node
                 {
